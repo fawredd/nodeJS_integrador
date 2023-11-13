a58bed63a40a828c41d5a7462224f738f01bb90f
fawredd <moore.marcos@gmail.com>
2023-11-13 20:50:54 -0300

    [FEAT] Backend

--- src/server.mjs
+++ src/server.mjs
@@ -8,13 +8,8 @@ app.use(express.json());
 // Páginas estáticas en directorio "public"
 app.use(express.static('public'));
 
-// API routes
-import apiRoutes from './server/routes/api';
-app.use('/api', apiRoutes);
-
-// Otras routes
-import otherRoutes from './server/routes/other';
-app.use('/other', otherRoutes);
+// Ruta raiz
+app.get('/', handleRootRoute);
 
 // Iniciar el servidor
 const port = process.env.PORT || 3000;

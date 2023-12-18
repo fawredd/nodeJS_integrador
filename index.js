9999929cf5fc6a0ea7390e03b76eb82dcd7c15c8
fawredd <moore.marcos@gmail.com>
2023-12-18 10:20:44 -0300

    Backend

--- index.js
+++ index.js
@@ -68,8 +68,8 @@ app.set('view engine', 'ejs')
 app.use(expressLayouts)
 app.set('layout', 'layouts/layout')
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+/* app.use(express.json()) */
 
 app.use(methodOverride('_method'))
 
